8f2991c7d1cda8f6738a4a9e3f4a00e3f33cf821
gabriel sousa <107716656+sousaz@users.noreply.github.com>
2023-11-23 08:15:57 -0300

    Add files via upload

--- api/options.js
+++ api/options.js
@@ -17,18 +17,9 @@ function filterRepetitiveOptions(data) {
 module.exports = {
     async loadOptions(req, res){
         const id = req.params.id
-        console.log(id);
         try {
-            const consulta = id ? await Consulta.find({id_ubs: id}, 'tipo') : await Consulta.find({}, 'tipo')
-            const ubs = await Ubs.find({}, 'nome')
-            const medico = id ? await Medico.find({id_ubs: id}, 'nome especialidade') : await Medico.find({}, 'nome especialidade')
-            const tipo = filterRepetitiveOptions(consulta)
-            const response = {
-                tipoConsulta: tipo,
-                nomeUbs: ubs,
-                medico: medico
-            }
-            res.status(200).json(response)
+            const medico = await Medico.find({id_ubs: id}, 'nome especialidade')
+            res.status(200).json(medico)
         } catch (error) {
             console.log(error);
             res.status(400).json({ msg: 'Erro na consulta'})
